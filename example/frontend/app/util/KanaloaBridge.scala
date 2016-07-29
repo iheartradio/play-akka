9dcac31df29ef5c7f96310fafcc15da9a9f02664
Kailuo Wang <kailuo.wang@gmail.com>
2016-07-29 16:31:46 -0400

    fixed example / updated kanaloa and compatibility bug

--- example/frontend/app/util/KanaloaBridge.scala
+++ example/frontend/app/util/KanaloaBridge.scala
@@ -3,15 +3,22 @@ package util
 import javax.inject.Inject
 
 import akka.actor.ActorSystem
+import akka.util.Timeout
 import api.ErrorResult
 import asobu.distributed.gateway.AbstractKanaloaBridge
 import kanaloa.reactive.dispatcher._
 import play.api.Configuration
+import KanaloaBridge._
+import concurrent.duration._
 
-class KanaloaBridge @Inject() (config: Configuration, system: ActorSystem) extends AbstractKanaloaBridge {
+class KanaloaBridge @Inject() (implicit configuration: Configuration, system: ActorSystem) extends AbstractKanaloaBridge {
 
   override protected def resultChecker: ResultChecker = {
     case e: ErrorResult[_] ⇒ Left(e.toString)
     case m                ⇒ Right(m)
   }
+}
+
+object KanaloaBridge {
+  implicit val timeout: Timeout = 30.seconds
 }
